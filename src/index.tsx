265c4ede2ca6d3789b983ae3b0e694ae1ea804e6
Axel Chalon <xaxel@protonmail.com>
2019-08-12 19:31:45 +0200

    WsProvider

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {WsProvider} from '@polkadot/api';
+import Api from '@polkadot/api/promise';
+
+Api.create({ provider: new WsProvider('wss://substrate-rpc.parity.io/')}).then((api: any) => {
+  api.rpc.chain.subscribeNewHead((header: any) => {
+  console.log(`new block #${header.number.toNumber()}`);
+  })
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
